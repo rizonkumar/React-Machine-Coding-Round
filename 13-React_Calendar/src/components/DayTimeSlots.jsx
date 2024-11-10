6287fecbf4935ec631b1c18d671c95c974d517fa
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-10 16:32:58 +0530

    Feat: Implemented Calendar Commit

--- 13-React_Calendar/src/components/DayTimeSlots.jsx
+++ 13-React_Calendar/src/components/DayTimeSlots.jsx
@@ -1,7 +1,17 @@
 function DayTimeSlots() {
   const slots = Array.from({ length: 24 }, (_, index) => index);
-  console.log("Slpts", slots);
-  return <div> </div>;
+
+  return (
+    <div>
+      {slots.map((slot, index) => {
+        return (
+          <div className="slot" key={index}>
+            {slot} : 00
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default DayTimeSlots;
--- 13-React_Calendar/src/components/DayView.jsx
+++ 13-React_Calendar/src/components/DayView.jsx
@@ -1,9 +1,13 @@
 import DayTimeSlots from "./DayTimeSlots.jsx";
+import Events from "./Events.jsx";
+import events from "../data/events.json";
 
 function DayView() {
   return (
-    <div>
+    <div className="calendar">
+      <div className="right-line"></div>
       <DayTimeSlots />
+      <Events events={events} />
     </div>
   );
 }
