acfece33678f31ac7200255fdc179214763bd9ea
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-17 09:23:49 +0530

    Feat: Implemented Tab System with UI

--- 17_React-Tab/src/App.jsx
+++ 17_React-Tab/src/App.jsx
@@ -1,5 +1,30 @@
+import Tabs from "./components/Tabs";
+import "./App.css";
+
 function App() {
-  return <></>;
+  const tabsData = [
+    {
+      label: "Profile",
+      content: <div>Profile Info Content</div>,
+    },
+    {
+      label: "Dashboard",
+      content: <div>Dashboard Content</div>,
+    },
+    {
+      label: "Settings",
+      content: <div>Settings Content</div>,
+    },
+    {
+      label: "Invoice",
+      content: <div>Invoice Content</div>,
+    },
+  ];
+  return (
+    <>
+      <Tabs data={tabsData} />
+    </>
+  );
 }
 
 export default App;
---
+++ 17_React-Tab/src/components/Tabs.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const Tabs = ({ data }) => {
+  const [currrentTabIdx, setCurrentTabIdx] = useState(0);
+
+  return (
+    <div className="tabs">
+      <div className="tabs__container">
+        {data.map((item, index) => (
+          <button
+            className="tabs__item"
+            key={index}
+            data-active={currrentTabIdx === index}
+            onClick={() => setCurrentTabIdx(index)}
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
+      <div className="tabs__content">{data[currrentTabIdx].content}</div>
+    </div>
+  );
+};
+
+export default Tabs;
