786ab6e4c9518cbdb23b575951f57ff5186cf104
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-02-28 08:43:41 +0530

    Feat: Calling List Components

---
+++ 22_React-File-Explore/README.md
@@ -0,0 +1,2 @@
+1. NEsted file folder and struture
+2. Add remove and rename functionality file and folder
--- 22_React-File-Explore/src/App.jsx
+++ 22_React-File-Explore/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { useState } from "react";
+import json from "./data/constant.json";
+import List from "./components/List";
 
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState(json);
+
+  return (
+    <div className="App">
+      <h1>File/Folder Explore</h1>
+      <List list={data} />
+    </div>
+  );
 };
 
 export default App;
