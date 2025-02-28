3bd3c8b5ddf2a1e6add5ef49ec43b7659b13ce91
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-02-28 08:38:03 +0530

    Feat: Rendering File and Folder with + funtionality

---
+++ 22_React-File-Explore/src/components/List.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const List = ({ list }) => {
+  const [open, setOpen] = useState(false);
+
+  console.log("List from List.jsx", list);
+  return (
+    <div className="container">
+      {list.map((item) => (
+        <div key={item.id}>
+          {item.isFolder && <span onClick={() => setOpen(!open)}>+</span>}
+          <span> {item.name}</span>
+          {open && item.children && <List list={item.children} />}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
