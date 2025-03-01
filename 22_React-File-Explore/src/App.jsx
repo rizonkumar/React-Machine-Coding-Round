249f395b3bbf2ed6a6559db5d71cbde09bf6fb1c
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-01 09:28:51 +0530

    Fea: Implemented Delete Node and Add Folder

--- 22_React-File-Explore/src/App.jsx
+++ 22_React-File-Explore/src/App.jsx
@@ -5,10 +5,56 @@ import List from "./components/List";
 const App = () => {
   const [data, setData] = useState(json);
 
+  const addFolder = (parentId) => {
+    const name = prompt("Enter folder name:");
+    const updateTree = (list) => {
+      return list.map((item) => {
+        if (item.id === parentId) {
+          return {
+            ...item,
+            children: [
+              ...item.children,
+              {
+                id: Math.random(),
+                name: name, // TODO: we need to ask user to enter name using input feild
+                isFolder: true, //TODO: we need to ask user to create folder/file.
+                children: [],
+              },
+            ],
+          };
+        }
+        if (item.children) {
+          return {
+            ...item,
+            children: updateTree(item.children),
+          };
+        }
+      });
+    };
+    setData((prev) => updateTree(prev));
+  };
+
+  const deleteNode = (itemId) => {
+    const updateTree = (list) => {
+      return list
+        .filter((item) => item.id !== itemId)
+        .map((item) => {
+          if (item.children) {
+            return {
+              ...item,
+              children: updateTree(item.children),
+            };
+          }
+          return item;
+        });
+    };
+    setData((prev) => updateTree(prev));
+  };
+
   return (
     <div className="App">
       <h1>File/Folder Explore</h1>
-      <List list={data} />
+      <List list={data} addFolder={addFolder} deleteNode={deleteNode} />
     </div>
   );
 };
--- 22_React-File-Explore/src/components/List.jsx
+++ 22_React-File-Explore/src/components/List.jsx
@@ -1,24 +1,48 @@
 import { useState } from "react";
 
-const List = ({ list }) => {
+const List = ({ list, addFolder, deleteNode }) => {
   const [open, setOpen] = useState({});
 
-  console.log("List from List.jsx", list);
   return (
     <div className="container">
       {list.map((item) => (
-        <div key={item.id}>
-          {item.isFolder && (
+        <div key={item?.id}>
+          {item?.isFolder && (
             <span
               onClick={() =>
-                setOpen((prev) => ({ ...prev, [item.id]: !prev[item.id] }))
+                setOpen((prev) => ({ ...prev, [item?.id]: !prev[item?.id] }))
               }
             >
-              {open?.[item.id] ? "▼" : "▶"}
+              {open?.[item?.id] ? "▼" : "▶"}
             </span>
           )}
-          <span> {item.name}</span>
-          {open?.[item.id] && item.children && <List list={item.children} />}
+          <span> {item?.name}</span>
+          {item?.isFolder && (
+            <span onClick={() => addFolder(item.id)}>
+              {" "}
+              <img
+                className="icon"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfMt43f5llkF5OgPwtIozkZk38jQu2r-3XCg&s"
+                alt={item?.name}
+              />{" "}
+            </span>
+          )}
+          <span>
+            <img
+              className="icon"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkTz_Rz5cU9HV9HqlgNg_jxYbkc-lQtdgYrA&s"
+              alt={item?.id}
+              onClick={() => deleteNode(item.id)}
+            />
+          </span>
+
+          {open?.[item?.id] && item?.children && (
+            <List
+              list={item?.children}
+              addFolder={addFolder}
+              deleteNode={deleteNode}
+            />
+          )}
         </div>
       ))}
     </div>
