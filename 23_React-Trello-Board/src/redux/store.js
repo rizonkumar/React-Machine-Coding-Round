4e7d933c59911bb297dd479da3a1ebe34551b83b
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-09 17:36:13 +0530

    Fix: configure store and react dom issue

--- 23_React-Trello-Board/src/redux/store.js
+++ 23_React-Trello-Board/src/redux/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducer";
 
-const store = configureStore(
+const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
