f2672f909847c8db13018fd82288e6add85e2021
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-09 17:48:42 +0530

    Feat: Implemented Trello List Component

--- 23_React-Trello-Board/src/components/Trello/TrelloList.jsx
+++ 23_React-Trello-Board/src/components/Trello/TrelloList.jsx
@@ -1,7 +1,80 @@
+import { useDispatch } from "react-redux";
 import "./css/TrelloList.css";
+import { useState } from "react";
+import TrelloCard from "./TrelloCard";
+import { addCard } from "../../redux/actions";
 
-const TrelloList = () => {
-  return <div>TrelloList</div>;
+const TrelloList = ({ list }) => {
+  console.log("TrelloList", list);
+  const dispatch = useDispatch();
+
+  const [showForm, setShowForm] = useState(false);
+  const [cardContent, setCardContent] = useState("");
+  const [showActions, setShowActions] = useState(false);
+
+  const toggleActions = () => {
+    setShowActions(!showActions);
+  };
+
+  const handleAddCard = () => {
+    if (cardContent.trim()) {
+      dispatch(addCard(list.id, cardContent));
+      setCardContent("");
+      setShowForm(false);
+    }
+  };
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+    setCardContent("");
+  };
+
+  return (
+    <div className="trello-list">
+      <div className="list-header">
+        <h2 className="list-title">{list?.title}</h2>
+        <button className="list-menu-button" onClick={toggleActions}>
+          ...
+        </button>
+        {showActions && (
+          <div className="list-actions">
+            {/* TODO: Implement List Actions */}
+            <p>Actions menu here</p>
+          </div>
+        )}
+      </div>
+
+      <div className="cards-container">
+        {list?.cards?.map((card) => (
+          <TrelloCard key={card.id} card={card} />
+        ))}
+      </div>
+
+      {showForm ? (
+        <div className="add-card-form">
+          <textarea
+            className="card-text-area"
+            placeholder="Enter a title for this card"
+            value={cardContent}
+            onChange={(e) => setCardContent(e.target.value)}
+            autoFocus
+          />
+          <div className="form-buttons">
+            <button className="add-button" onClick={handleAddCard}>
+              Add Card
+            </button>
+            <button className="cancel-button" onClick={toggleForm}>
+              x
+            </button>
+          </div>
+        </div>
+      ) : (
+        <button className="add-card-button" onClick={toggleForm}>
+          + Add a card
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default TrelloList;
