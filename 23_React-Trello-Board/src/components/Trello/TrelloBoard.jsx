ddea884279aabe7b94c21afbff3138f63c52be44
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-09 17:49:56 +0530

    Feat: get list from the redux

--- 23_React-Trello-Board/src/components/Trello/TrelloBoard.jsx
+++ 23_React-Trello-Board/src/components/Trello/TrelloBoard.jsx
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux";
 import TrelloList from "./TrelloList";
 import "./css/TrelloBoard.css";
 
 const TrelloBoard = () => {
-  const lists = []; // TODO: Connect to redux
+  const lists = useSelector((state) => state.lists);
   return (
     <div className="trello-board">
       <div className="lists-container">
         {lists.map((list) => (
           <TrelloList key={list.id} list={list} />
         ))}
+        {/* TODO: Implement AddList */}
+        <div className="add-list-placeholder">+ Add another list</div>
       </div>
     </div>
   );
