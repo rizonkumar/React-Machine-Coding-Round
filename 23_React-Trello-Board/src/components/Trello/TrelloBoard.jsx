ca1aa7535e974c0ae0aca0243d07c5f1386c5813
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-10 08:49:14 +0530

    Feat: Implemented Add List component and called the AddList in the Board

--- 23_React-Trello-Board/src/components/Trello/TrelloBoard.jsx
+++ 23_React-Trello-Board/src/components/Trello/TrelloBoard.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
 import TrelloList from "./TrelloList";
 import "./css/TrelloBoard.css";
+import AddList from "../AddList";
 
 const TrelloBoard = () => {
   const lists = useSelector((state) => state.lists);
+
   return (
     <div className="trello-board">
       <div className="lists-container">
         {lists.map((list) => (
           <TrelloList key={list.id} list={list} />
         ))}
-        {/* TODO: Implement AddList */}
-        <div className="add-list-placeholder">+ Add another list</div>
+        <AddList />.
       </div>
     </div>
   );
