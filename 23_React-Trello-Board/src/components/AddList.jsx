0c9b22340a6a673b78b31f2353c5a5973a52a3e3
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-03-11 08:12:24 +0530

    Feat: Implemented List Actions components

--- 23_React-Trello-Board/src/components/AddList.jsx
+++ 23_React-Trello-Board/src/components/AddList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addList } from "../redux/actions";
+import "./Trello/css/AddList.css";
 
 const AddList = () => {
   const dispatch = useDispatch();
