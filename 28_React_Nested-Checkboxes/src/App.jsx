0d89eb52d70614237e0edea77be70279c861450d
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-04-05 18:12:50 +0530

    Feat: Implemented Checkboxes tick and untick

--- 28_React_Nested-Checkboxes/src/App.jsx
+++ 28_React_Nested-Checkboxes/src/App.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { checkBoxesData } from "./data/constants";
+
+const CheckBoxes = ({ data, checked, setChecked }) => {
+  const handleChange = (e, id) => {
+    setChecked((prev) => {
+      const newState = { ...prev, [id]: e.target.checked };
+      return newState;
+    });
+  };
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id} className="parent">
+          <input
+            type="checkbox"
+            id={item.id}
+            checked={checked[item?.id] || false}
+            onChange={(e) => handleChange(e, item.id)}
+          />
+          <span>{item.name}</span>
+          {item.children && (
+            <CheckBoxes
+              data={item?.children}
+              checked={checked}
+              setChecked={setChecked}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const App = () => {
-  return <div>App</div>;
+  const [checked, setChecked] = useState({});
+  return (
+    <div>
+      <CheckBoxes
+        data={checkBoxesData}
+        checked={checked}
+        setChecked={setChecked}
+      />
+    </div>
+  );
 };
 
 export default App;
