659c01e54d2b0a72d2309e5ac4df7d96e303e5f9
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-04-07 21:36:29 +0530

    Feat: Project Setup

---
+++ 29_React_Typing-Effect/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TypingEffect from "./components/TypingEffect";
+
+const App = () => {
+  return (
+    <div>
+      <TypingEffect text="I'm a FullStack Developer" delay="100" />
+    </div>
+  );
+};
+
+export default App;
